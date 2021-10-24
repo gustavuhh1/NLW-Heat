88abcd88f0e1b9333d2d6ffcc223cbc9141c4c17
Gustavo Martins Rodrigues <gustavomrodrigues11@gmail.com>
2021-10-24 13:43:57 -0300

    changing facebook

--- main.js
+++ main.js
@@ -1,36 +1,33 @@
- const linksSocialMedia = {
-        github: "gustavuhh1",
-        youtube: "channel/UCty2PUetiTktwjvMFQeWTDw",
-        facebook: "maykbrito",
-        instagram: "gustavomrodrigues11",
-        twitter: "Gustavuhh_1"
-      }
+const linksSocialMedia = {
+  github: "gustavuhh1",
+  youtube: "channel/UCty2PUetiTktwjvMFQeWTDw",
+  facebook: "profile.php?id=100026030179976",
+  instagram: "gustavomrodrigues11",
+  twitter: "Gustavuhh_1",
+};
 
-function changeSocialMediaLinks(){
-    for(let li of socialLinks.children){
-    const social = li.getAttribute('class')
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
 
-    li.children[0].href = `http://${social}.com/${linksSocialMedia[social]}`
-    }
+    li.children[0].href = `http://${social}.com/${linksSocialMedia[social]}`;
+  }
 }
 
-changeSocialMediaLinks()
+changeSocialMediaLinks();
 
-function getGitHubProfileInfos(){
-    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`;
 
-    fetch(url)
-    .then(response => response.json() )
-    .then(data => {
-        userName.textContent = data.name;
-        userBio.textContent = data.bio;
-        userLink.href = data.html_url;
-        userImage.src = data.avatar_url;
-        userLogin.textContent = data.login;
-    })
-
-
-
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
 }
 // GG Project
-getGitHubProfileInfos()
+getGitHubProfileInfos();
